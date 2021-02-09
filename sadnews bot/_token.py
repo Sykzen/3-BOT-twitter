5cf1bf5421faf4723a741ee0221661857ddb0776
sykzen <sykzen@hotmail.com>
2021-02-09 10:05:49 +0100

    fix : gitignore will ignore token nd login file

--- sadnews bot/_token.py
+++
@@ -1,9 +0,0 @@
-
-#Token that you've got in twitter/developpers
-API_KEYS="UX4iX4wGzve8k0NZvcB08p6Ip"
-Bearer_token="AAAAAAAAAAAAAAAAAAAAAIVMMAEAAAAAt9q%2Bt3k%2FLXc%2BChpbPz3K9J%2BsDEE%3DdfPN72kPdT5Znvs8sTFoO0Qmt3Lz5yir4mHpJ7bTe2u7KwJZ8J"
-API_SECRET_KEYS="L7eInYMaGM5GKX6hwAS13Ms1N1hhNHX6Fyqdc09y0oqRGs2xRc"
-TOKEN="2866131187-3nCiTQGBoNRuFOcuwlUn0jLYiXfyF36m75GXc9M"
-TOKEN_SECRET="iAhVHHc0n289JRVQrO4tWA3KLOPtfy6QBGaFjHzF2aAxF"
-#syntaxe to use to call the bot ( for exemple : Sykzen=help to call help doc in this case)
-prefixeBot = "Sykzen="
